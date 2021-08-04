daccfcdb1230ca9412893c02bf65063ae6876684
zht7441 <87973252+zht7441@users.noreply.github.com>
2021-08-05 00:57:41 +0800

    现在category主界面显示各个category和其page

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -7,3 +7,10 @@ register = template.Library()
 def get_category_list(current_category=None):
     return {'categories': Category.objects.all(),
         'current_category': current_category}
+
+@register.inclusion_tag('rango/pages.html')
+def get_page_list(current_category=None,current_page=None):
+    return {'current_category': current_category,
+            'pages': Page.objects.filter(category=current_category),
+            'current_page': current_page,
+            }
